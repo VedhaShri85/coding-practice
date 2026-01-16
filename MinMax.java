e374ce000b80688b56381a25e79c3436fad8bc62
VedhaShri85 <vedhashriraam@gmail.com>
2026-01-16 22:23:04 +0530

    Update MinMax.java

--- MinMax.java
+++ MinMax.java
@@ -3,21 +3,18 @@ import java.util.*;
 class MinMax {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        // Read size of array
+        
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        // Read array elements
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        // Initialize min and max
         int min = arr[0];
         int max = arr[0];
 
-        // Find min and max
         for (int i = 1; i < n; i++) {
             if (arr[i] < min) {
                 min = arr[i];
@@ -27,7 +24,8 @@ class MinMax {
             }
         }
 
-        // Output result
+     
         System.out.println(min + " " + max);
     }
 }
+       // problem source: geeksforgeeks
