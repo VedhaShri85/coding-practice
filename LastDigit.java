3147d5b8a7e0e75da55aba7389c7f168f9d23194
VedhaShri85 <vedhashriraam@gmail.com>
2026-01-19 22:40:03 +0530

    Update LastDigit.java

--- LastDigit.java
+++ LastDigit.java
@@ -1,30 +1,22 @@
 class LastDigit {
     static int getLastDigit(String a, String b) {
 
-        // If b == "0", a^0 = 1
         if (b.equals("0")) {
             return 1;
         }
-
-        // Last digit of base a
         int lastDigit = a.charAt(a.length() - 1) - '0';
-
-        // Find b % 4 (since cycle length is 4)
         int mod = 0;
         for (int i = 0; i < b.length(); i++) {
             mod = (mod * 10 + (b.charAt(i) - '0')) % 4;
         }
-
-        // If mod is 0, treat it as 4
         if (mod == 0) {
             mod = 4;
         }
-
         int result = 1;
         for (int i = 0; i < mod; i++) {
             result = (result * lastDigit) % 10;
         }
-
         return result;
     }
 }
+
