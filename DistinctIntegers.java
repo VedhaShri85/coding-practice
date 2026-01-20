2ef8cee1f70fb7b2ba4bbd22ed64a02d67c739cc
VedhaShri85 <vedhashriraam@gmail.com>
2026-01-20 22:17:03 +0530

    Add files via upload

---
+++ DistinctIntegers.java
@@ -0,0 +1,13 @@
+class DistinctIntegers {
+    public int distinctIntegers(int n) {
+        if(n==1){
+            return 1;
+        }
+        else{
+
+        
+        return (n-1);
+        }
+    }
+}
+// leetcode 2549
