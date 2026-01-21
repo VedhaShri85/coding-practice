32cd44d30122f5a0062116666dfc42f940578f8e
VedhaShri85 <vedhashriraam@gmail.com>
2026-01-21 23:26:34 +0530

    Add files via upload

---
+++ RevExponentiation.java
@@ -0,0 +1,20 @@
+class RevExponentiation {
+    static int reverseExponentiation(int n) {
+        int rev = 0;
+        int temp = n;
+
+        while (temp > 0) {
+            rev = rev * 10 + (temp % 10);
+            temp = temp / 10;
+        }
+
+        int result = 1;
+
+        for (int i = 0; i < rev; i++) {
+            result *= n;
+        }
+
+        return result;
+    }
+}
+
