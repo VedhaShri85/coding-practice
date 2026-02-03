c699909f070cf1101a4c9447a3e4edfb66f955a3
VedhaShri85 <vedhashriraam@gmail.com>
2026-02-03 23:34:00 +0530

    Create replacezerowithone.java

---
+++ replacezerowithone.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ReplaceZeroWithOne {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int result = 0;
+        int place = 1;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit == 0) {
+                digit = 1;
+            }
+
+            result = digit * place + result;
+            place *= 10;
+            n = n / 10;
+        }
+
+        System.out.println(result);
+    }
+}
