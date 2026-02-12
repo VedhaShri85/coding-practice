1e85105b54d769e88566a6c664721647d942298f
VedhaShri85 <vedhashriraam@gmail.com>
2026-02-12 22:20:55 +0530

    Create ArrayMissingNum.java

---
+++ ArrayMissingNum.java
@@ -0,0 +1,12 @@
+class Solution {
+    static int missingNumber(int arr[], int n) {
+
+        int total = n * (n + 1) / 2;
+        int sum = 0;
+        for (int i = 0; i < n - 1; i++) {
+            sum += arr[i];
+        }
+
+        return total - sum;
+    }
+}
