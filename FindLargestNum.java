9d1a5a1c2c16ab9133354e6057f8b20a4a1bf60d
VedhaShri85 <vedhashriraam@gmail.com>
2026-02-23 21:22:05 +0530

    Create FindLargestNum.java

---
+++ FindLargestNum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int max=Integer.MIN_VALUE;
+		for(int i=1;i<n;i++){
+		    int num=sc.nextInt();
+		    if(num>max){
+		        max=num;
+		    }
+		}
+		System.out.print(max);
+	}
+}
